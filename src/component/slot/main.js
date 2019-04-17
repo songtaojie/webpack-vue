1373bdb13ebc5da439b15e38f1c2342d4c5151e8
songtaojie <stjworkemail@163.com>
2019-04-17 18:45:55 +0800

    组件信息

--- src/component/slot/main.js
+++ src/component/slot/main.js
@@ -5,30 +5,58 @@
 // import Vue from "../node_modules/vue/dist/vue.js";
  import Vue from "vue/dist/vue.js";
 //.sync修饰符
-Vue.component('alert-box',{
-    data(){
-        return {
-            'success':{
-                color: '#3c763d',
-                'background-color': '#dff0d8',
-                'border-color': '#d6e9c6',
-            }
-        }
+// Vue.component('alert-box',{
+//     data(){
+//         return {
+//             'success':{
+//                 color: '#3c763d',
+//                 'background-color': '#dff0d8',
+//                 'border-color': '#d6e9c6',
+//             }
+//         }
+//     },
+//     template:`<div  v-bind='$attrs'>
+//     <div :style=""></div>
+//         <strong>{{$attrs.type=='success'?'成功!':'失败!'}}</strong>
+//         <slot></slot>
+//     </div>`,
+//     methods:{
+//         hide(){
+//             this.$emit('update:uphide',false);
+//         }
+//     },
+//     mounted(){
+//         console.log(this.props);
+//         console.log(this.$attrs)
+//     }
+// });
+// var vm = new Vue({
+//     el:'#app',
+//     data(){
+//         return {
+//             isShow:false
+//         }
+//     },
+//     methods:{
+//         show(){
+//             this.isShow = true;
+//         },
+//         hide(value){
+//             debugger
+//             this.isShow = value;
+//         }
+//     }
+// });
+Vue.component('navigation-link',{
+    props:{
+        url:String,
+        newPage:Boolean
     },
-    template:`<div  v-bind='$attrs'>
-    <div :style=""></div>
-        <strong>{{$attrs.type=='success'?'成功!':'失败!'}}</strong>
+    template:`<div>
+        <a :href='url'>
         <slot></slot>
-    </div>`,
-    methods:{
-        hide(){
-            this.$emit('update:uphide',false);
-        }
-    },
-    mounted(){
-        console.log(this.props);
-        console.log(this.$attrs)
-    }
+        </a>
+    </div>`
 });
 var vm = new Vue({
     el:'#app',
